22758458bc506abb812badf454b9e973dbe3157a
Mumbai <zeessuzeessu84@gmail.com>
2025-12-25 00:07:13 +0530

    Create deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Coffee = await hre.ethers.getContractFactory("Coffee");
+  const coffee = await Coffee.deploy();
+
+  await coffee.deployed();
+
+  console.log("Coffee Contract deployed to:", coffee.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
