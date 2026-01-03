f2dc516f5c24c6fe48536a97ebefdb33e3ba1fd9
Mumbai <zeessuzeessu84@gmail.com>
2026-01-03 18:05:59 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,8 +2,7 @@ hhcfgconst hre = require("hardhat");
 
 async function main() {
   const Coffee = await hre.ethers.getContractFactory("Coffee");
-  const coffee = await Coffee.deploy();
-
+  const coffee = await Coffee.deploy(
   await coffee.deployed
   console.log("Coffee Contract deployed to:", coffee.addres
 main().catch((error) 
