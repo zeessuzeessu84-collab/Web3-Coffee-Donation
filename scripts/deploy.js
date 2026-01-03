dffef9b5f7612d1d795bd3378a18dfea7686c76e
Mumbai <zeessuzeessu84@gmail.com>
2026-01-03 18:15:31 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-yytttggghuucffdffrtttfdffghhhcfgconst hre = require("hardhat");
+vgyytttggghuucffdffrtttfdffghhhcfgconst hre = require("hardhat");
 
 async function main() {
   const Coffee = await hre.ethers.getContractFactory("Coffee");
